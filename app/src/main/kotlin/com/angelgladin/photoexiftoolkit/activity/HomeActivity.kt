5fc088907802b5bbbdfe16eb1a7cb9fcd4ff8368
angelgladin <an098@hotmail.com>
2016-12-22 17:10:38 -0600

    Create simple UI for Home screen :squirrel:

--- app/src/main/kotlin/com/angelgladin/photoexiftoolkit/activity/HomeActivity.kt
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/activity/HomeActivity.kt
@@ -1,16 +1,30 @@
 package com.angelgladin.photoexiftoolkit.activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.angelgladin.photoexiftoolkit.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
-  //private val toolbar by lazy { findViewById(R.id.toolbar) as Toolbar? }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.activity_home)
+    setSupportActionBar(toolbar)
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    menuInflater.inflate(R.menu.menu_main, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    val id = item.itemId
+    if (id == R.id.action_about_app) {
+      return true
+    }
+    return super.onOptionsItemSelected(item)
   }
 }
