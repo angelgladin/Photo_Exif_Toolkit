5136ff0f424dd1d0b467923226c607a3160f3667
angelgladin <an098@hotmail.com>
2016-12-22 19:15:51 -0600

    Add ExifInterface extension :squirrel:

--- app/src/main/kotlin/com/angelgladin/photoexiftoolkit/extension/ExifInterfaceExtension.kt
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/extension/ExifInterfaceExtension.kt
@@ -1,5 +1,22 @@
 package com.angelgladin.photoexiftoolkit.extension
 
+import android.media.ExifInterface
+
 /**
  * Created on 12/22/16.
  */
+fun ExifInterface.getMap(): MutableMap<String, String>? {
+  val mAttributesField = this.javaClass.getDeclaredField("mAttributes")
+  mAttributesField.isAccessible = true
+
+  @Suppress("UNCHECKED_CAST")
+  val map: MutableMap<String, String> = mAttributesField.get(this) as MutableMap<String, String>
+
+  val latLonArray = FloatArray(2)
+  if (this.getLatLong(latLonArray)) {
+    map["Latitude"] = latLonArray[0].toString()
+    map["Longitude"] = latLonArray[1].toString()
+  }
+
+  return map
+}
