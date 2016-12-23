4a9fe8d68947a72cd698ee4b574e083272d72503
angelgladin <an098@hotmail.com>
2016-12-22 22:19:14 -0600

    Make ExifField Serializable :squirrel:

--- app/src/main/kotlin/com/angelgladin/photoexiftoolkit/domain/ExifField.kt
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/domain/ExifField.kt
@@ -1,6 +1,8 @@
 package com.angelgladin.photoexiftoolkit.domain
 
+import java.io.Serializable
+
 /**
  * Created on 12/22/16.
  */
-data class ExifField(val tag: String, val attribute: String)
+data class ExifField(val tag: String, val attribute: String) : Serializable
