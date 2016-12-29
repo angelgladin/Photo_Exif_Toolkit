9d1afb748574cc737804122d94afea1f4d6c864d
angelgladin <an098@hotmail.com>
2016-12-28 22:58:41 -0600

    Use object instead of class :squirrel:

--- app/src/main/kotlin/com/angelgladin/photoexiftoolkit/util/Constants.kt
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/util/Constants.kt
@@ -3,13 +3,11 @@ package com.angelgladin.photoexiftoolkit.util
 /**
  * Created on 12/24/16.
  */
-class Constants {
-    companion object {
-        ///EXIF CONSTANTS///
-        val EXIF_LATITUDE = "Latitude"
-        val EXIF_LONGITUDE = "Longitude"
+object Constants {
+    ///EXIF CONSTANTS///
+    val EXIF_LATITUDE = "Latitude"
+    val EXIF_LONGITUDE = "Longitude"
 
-        ///INTENTS KEYS///
-        val PATH_FILE_KEY = "path_file"
-    }
+    ///INTENTS KEYS///
+    val PATH_FILE_KEY = "path_file"
 }
