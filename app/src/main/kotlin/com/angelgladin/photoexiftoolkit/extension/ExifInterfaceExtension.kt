9f8e5f50014193e450819ddb2ac90fe0a2e91879
angelgladin <an098@hotmail.com>
2016-12-28 22:58:08 -0600

    Add extensions to ExifInterface :squirrel:

--- app/src/main/kotlin/com/angelgladin/photoexiftoolkit/extension/ExifInterfaceExtension.kt
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/extension/ExifInterfaceExtension.kt
@@ -21,3 +21,22 @@ fun ExifInterface.getMap(): MutableMap<String, String> {
 
     return map
 }
+
+fun convertDecimalToDegrees(decimal: Double): String {
+    var latitude = Math.abs(decimal)
+    val degree = latitude.toInt()
+    latitude *= 60
+    latitude -= (degree * 60.0)
+    val minute = latitude.toInt()
+    latitude *= 60
+    latitude -= (minute * 60.0)
+    val second = (latitude * 1000.0).toInt()
+    return "$degree/1,$minute/1,$second/1000"
+}
+
+fun ExifInterface.getLatitudeRef(latitude: Double): String =
+        if (latitude < 0.0) "S" else "N"
+
+fun ExifInterface.getLongitudeRef(latitude: Double): String =
+        if (latitude < 0.0) "W" else "E"
+
