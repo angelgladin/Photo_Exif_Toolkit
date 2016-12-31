d0edbfb4055411bcd21b6f116cef73438fc29448
angelgladin <an098@hotmail.com>
2016-12-31 00:02:45 -0600

    Add API constants :squirrel:

---
+++ app/src/main/kotlin/com/angelgladin/photoexiftoolkit/data/ApiConstants.kt
@@ -0,0 +1,12 @@
+package com.angelgladin.photoexiftoolkit.data
+
+/**
+ * Created on 12/30/16.
+ */
+object ApiConstants {
+
+    const val BASE_URL = "https://maps.googleapis.com/"
+
+    const val ADDRESS_URL = "maps/api/geocode/json?sensor=true"
+    const val LATLNG_QUERY = "latlng"
+}
